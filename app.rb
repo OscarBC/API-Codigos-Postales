81955c071a35629af3c110c94d56d00111a13916
Oscar Barrera <oscar.barrera@rocktech.mx>
2020-07-13 18:40:18 -0500

    [UPDATE] - remove auth

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ Cuba.define do
     end
 
     on 'codigo_postal/:codigo_postal' do |codigo_postal|
-      env['warden'].authenticate!(:token)
       res.headers['Cache-Control'] = 'max-age=525600, public'
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
       res.headers['Access-Control-Allow-Origin'] = '*'
@@ -15,7 +14,6 @@ Cuba.define do
     end
 
     on 'buscar', param('q') do |query|
-      env['warden'].authenticate!(:token)
       res.headers['Cache-Control'] = 'max-age=525600, public'
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
       res.headers['Access-Control-Allow-Origin'] = '*'
@@ -26,7 +24,6 @@ Cuba.define do
     end
 
     on 'v2/codigo_postal/:codigo_postal' do |codigo_postal|
-      env['warden'].authenticate!(:token)
       res.headers['Cache-Control'] = 'max-age=525600, public'
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
       res.headers['Access-Control-Allow-Origin'] = '*'
@@ -34,7 +31,6 @@ Cuba.define do
     end
 
     on 'v2/buscar', param('codigo_postal') do |codigo_postal|
-      env['warden'].authenticate!(:token)
       res.headers['Cache-Control'] = 'max-age=525600, public'
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
       res.headers['Access-Control-Allow-Origin'] = '*'
